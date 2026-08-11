45e1b8aa2ef3e290b5950bb939224b0f7145832d
hoangtri30d1973 <hmt30d1973@gmail.com>
2026-08-11 15:38:25 +0700

    Add password protection for project access

---
+++ auth.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+PROJECT_PASSWORD = "hmt1236"
+
+
+def check_project_password():
+
+    if st.session_state.get("project_authenticated"):
+        return True
+
+    st.title("🔒 Protected Page")
+
+    password = st.text_input(
+        "Enter password",
+        type="password"
+    )
+
+    if st.button("Unlock"):
+
+        if password == PROJECT_PASSWORD:
+
+            st.session_state[
+                "project_authenticated"
+            ] = True
+
+            st.rerun()
+
+        else:
+
+            st.error(
+                "Wrong password"
+            )
+
+    return False
