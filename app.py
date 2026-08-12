59855c5226fc91551543b5ecb0b9b65f2f5d3d1a
hoangtri30d1973 <hmt30d1973@gmail.com>
2026-08-12 13:31:08 +0700

    Refactor app.py to restore original content

--- app.py
+++ app.py
@@ -21,15 +21,15 @@ st.markdown("""
 ## Đột phá
 - Cảm giác giống như nhốt mình trong một căn phòng tối om, bên trong phòng chỉ có một cửa sổ sáng mịt mờ. Gục trước cửa sổ tựa hồ có thể mông lung cảm nhận được thế giới bên ngoài, phảng phất có núi, có cây, có người, còn có hoa thơm bướm lượn, thậm chí có tiếng nước chảy rào rào, làm cho ngươi vô cùng nôn nóng _muốn ra ngoài, nhìn thế giới_ rút cuộc như thế nào. 
 - Nhưng ta lại không cách nào đâm phá được chiếc cửa sổ kia, tựa như cửa sổ được làm bằng da trâu gân hồ siêu dai, bất luận dùng biện pháp gì cũng không thể đâm được. Có đôi khi cảm giác phảng phất đã đạt đến đột phá, phảng phất có thể dễ dàng đâm thủng chiếc cửa sổ kia, nhưng cuối cùng lại vỡ mộng, làm cho người ta phát điên, tinh thần uể oải. 
-- Đây là sự *lột xác* từ con sâu lông thành con bướm. Trải quá từng bước bò trườn chậm chạp, kinh qua sự từng trải và tích lũy dài lâu, sau đó yên tĩnh trở lại lắng đọng nội liễm kết thành cái kén, lẳng lặng chờ đợi ngày phá kén trùng sinh.
+- Đây là sự **lột xác** từ con sâu lông thành con bướm. Trải quá từng bước bò trườn chậm chạp, kinh qua sự từng trải và tích lũy dài lâu, sau đó yên tĩnh trở lại lắng đọng nội liễm kết thành cái kén, lẳng lặng chờ đợi ngày phá kén trùng sinh.
 
 ## Dụng binh
 - Phép dụng binh, là lấy mưu làm chính! 
     - Đó là dùng mưu lược trận, trước tiên phải mưu đoạt địa lợi; muốn dùng mưu thắng địch, trước tiên phải dùng mưu đặt bản thân vào thế bất bại.
 - Dũng cảm hay nhát gan là tại phương pháp, thành hay bại là tại trí. 
     - Nghĩa là dũng cảm hay nhát gan nằm ở mưu, mạnh hay yếu nằm ở thế. 
-    - Mưu đạt, việc thành thì làm người dũng. 
-    - Mưu hỏng, thế mất thì làm người nhát gan giả dũng cảm.
+        - Mưu đạt, việc thành thì làm người dũng. 
+        - Mưu hỏng, thế mất thì làm người nhát gan giả dũng cảm.
 - Nước có nội loạn, không thể xuất binh; lòng quân không yên không thể ra trận; trận thế không chắc chắn, thì không thể đánh; không đồng lòng đánh, tất không thể quyết chiến.
 
 ## Sinh mệnh
@@ -41,7 +41,7 @@ st.markdown("""
 - Trong cuộc sống, những chuyện ta không mong muốn vẫn luôn xảy ra. 
     - Chẳng hạn, nhỏ thì như chuyện: cơm sống, canh mặn; hoặc 
     - Lớn hơn như chuyện: trẻ con hàng xóm đánh nhau hay anh chồng nhậu say xỉn… 
-    *Bản thân chúng chưa phải là vấn đề; chính thái độ và cách xử lý không thích hợp của chúng ta mới thực sự biến chúng thành vấn đề.*
+        **Bản thân chúng chưa phải là vấn đề; chính thái độ và cách xử lý không thích hợp của chúng ta mới thực sự biến chúng thành vấn đề.**
 - Sáng tạo bất chiếm hữu, Thành công bất tự cứ. 
 
 ## Sự rèn luyện
